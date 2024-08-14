449ebba5781b68329e04d445cf165095fd8e6d91
Muzaffar2807 <53538056+Muzaffar2807@users.noreply.github.com>
2024-08-14 12:33:25 +0530

    added custom web compoenntns

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-import "./MyComponent.css"
+import "./MyComponent.css";
+import reactToWebComponent from "react-to-webcomponent";
+import { defineCustomElements } from "@webcomponents/custom-elements";
 
+// The original React component
 const MyComponent = ({ text }) => (
   <div className="main-container">
     <h1 className="text-color">{text}</h1>
   </div>
 );
- 
 
 MyComponent.propTypes = {
   text: PropTypes.string.isRequired,
 };
-
-export default MyComponent;
  
+const MyWebComponent = reactToWebComponent(MyComponent, React, ReactDOM);
+ 
+customElements.define("my-web-component", MyWebComponent);
+ 
+defineCustomElements(window);
